c3d732cd1525a49e6bc404c1e6e717de0b025398
Evan Krause <evangrayk@gmail.com>
2017-01-14 15:20:07 -0800

    add parser token test

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
+#include <sstream>
 
 TEST(NginxConfigParserTest, SimpleConfig) {
   NginxConfigParser parser;
@@ -8,4 +9,23 @@ TEST(NginxConfigParserTest, SimpleConfig) {
   bool success = parser.Parse("example_config", &out_config);
 
   EXPECT_TRUE(success);
+}
+
+TEST(NginxConfigParserTest, StatementParsed) {
+  NginxConfigParser parser;
+  NginxConfig config;
+
+  std::stringstream test_config_stream("port 8080;");
+
+  bool success = parser.Parse(&test_config_stream, &config);
+
+  EXPECT_TRUE(success);
+
+  // one statement
+  ASSERT_EQ(config.statements_.size(), 1);
+  // two tokens
+  ASSERT_EQ(config.statements_[0]->tokens_.size(), 2);
+
+  EXPECT_EQ(config.statements_[0]->tokens_[0], "port");
+  EXPECT_EQ(config.statements_[0]->tokens_[1], "8080");
 }
