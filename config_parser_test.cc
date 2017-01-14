d00295710d5efa95c73feba0c515d621c13b295f
Evan Krause <evangrayk@gmail.com>
2017-01-14 15:27:14 -0800

    add config parser whitepsace test

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -29,3 +29,22 @@ TEST(NginxConfigParserTest, StatementParsed) {
   EXPECT_EQ(config.statements_[0]->tokens_[0], "port");
   EXPECT_EQ(config.statements_[0]->tokens_[1], "8080");
 }
+
+TEST(NginxConfigParserTest, InsignificantWhitespace) {
+  NginxConfigParser parser;
+  NginxConfig config;
+
+  std::stringstream test_config_stream(" \t   foo  \t   bar     ;    \t");
+
+  bool success = parser.Parse(&test_config_stream, &config);
+
+  EXPECT_TRUE(success);
+
+  // one statement
+  ASSERT_EQ(config.statements_.size(), 1);
+  // two tokens
+  ASSERT_EQ(config.statements_[0]->tokens_.size(), 2);
+
+  EXPECT_EQ(config.statements_[0]->tokens_[0], "foo");
+  EXPECT_EQ(config.statements_[0]->tokens_[1], "bar");
+}
