8009851c0d7632a5518af863386c59cd93c8778a
Evan Krause <evangrayk@gmail.com>
2017-01-14 16:59:39 -0800

    add two statement parser test

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -78,3 +78,26 @@ TEST(NginxConfigParserTest, UnexpectedNewline) {
   EXPECT_EQ(config.statements_[0]->tokens_[0], "foo");
   EXPECT_EQ(config.statements_[0]->tokens_[1], "bar");
 }
+
+TEST(NginxConfigParserTest, OneLineTwoStatements) {
+  NginxConfigParser parser;
+  NginxConfig config;
+
+  std::stringstream test_config_stream("foo bar; baz bop;");
+
+  bool success = parser.Parse(&test_config_stream, &config);
+
+  EXPECT_TRUE(success);
+
+  // two statements
+  ASSERT_EQ(config.statements_.size(), 2);
+  // two tokens each
+  ASSERT_EQ(config.statements_[0]->tokens_.size(), 2);
+  ASSERT_EQ(config.statements_[1]->tokens_.size(), 2);
+
+  EXPECT_EQ(config.statements_[0]->tokens_[0], "foo");
+  EXPECT_EQ(config.statements_[0]->tokens_[1], "bar");
+
+  EXPECT_EQ(config.statements_[1]->tokens_[0], "baz");
+  EXPECT_EQ(config.statements_[1]->tokens_[1], "bop");
+}
